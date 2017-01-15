6fff6991b16fa773c93692923bef1f56ed8cc6e4
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2017-01-15 21:12:17 +0100

    2. App is vulnerable due to missing function level access control
    Visit http://localhost:8000/secret-info/ to exploit.

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ from django.views import defaults as default_views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    url(r'^secret-info/$', TemplateView.as_view(template_name='pages/secret-info.html'), name='secret'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
